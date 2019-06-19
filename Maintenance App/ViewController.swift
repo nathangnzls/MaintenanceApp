c735baf8b719059441b6af3e36fc115732b88d36
Nathaniel Gonzales <nathaniel.gonzales@robinsonsretail.com.ph>
2019-06-19 09:31:18 +0800

    initial

---
+++ Maintenance App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Maintenance App
+//
+//  Created by AndroidDev on 02/05/2018.
+//  Copyright © 2018 Esco Philippines Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
